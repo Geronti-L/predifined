a44c2ceabe3f870171a605abc37279c5dfe17b2a
Geronti-L <gerontiliparteliani@gmail.com>
2025-01-25 03:04:40 -0500

    __

--- src/test/java/predefined/PredefinedSteps.java
+++ src/test/java/predefined/PredefinedSteps.java
@@ -1,11 +1,67 @@
 package predefined;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 import static hooks.DriverManager.getDriver;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PredefinedSteps {
-    @Given("user navigates to {string}")
-    public void user_navigates_to(String url) {
+    @Given("I navigate to {string}")
+    public void I_navigate_to(String url) {
         getDriver().get(url);
+    }
+
+    @Then("title should be {string}")
+    public void titleShouldBe(String title) throws InterruptedException {
+      String actualTitle= getDriver().getTitle().toString();
+       assertThat(actualTitle,is(title));
+
+
+
+    }
+
+    @And("element with xpath {string} should be present")
+    public void elementWithXpathShouldBePresent(String xpath) throws InterruptedException {
+    getDriver().findElement(By.xpath(xpath)).isDisplayed();
+
+
+    }
+
+    @When("I type {string} into element with xpath {string}")
+    public void iTypeIntoElementWithXpath(String searchWord, String xpath) throws InterruptedException {
+        getDriver().findElement(By.xpath(xpath)).sendKeys(searchWord);
+        Thread.sleep(300);
+
+    }
+
+    @And("I click on element with xpath {string}")
+    public void iClickOnElementWithXpath(String xpath) throws InterruptedException {
+        getDriver().findElement(By.xpath(xpath)).click();
+        Thread.sleep(30000);
+    }
+
+    @And("I wait for element with xpath {string} to be present")
+    public void iWaitForElementWithXpathToBePresent(String xpath) throws InterruptedException {
+        getDriver().findElement(By.xpath(xpath)).isDisplayed();
+
+    }
+
+    @Then("element with xpath {string} should contain text {string}")
+    public void elementWithXpathShouldContainText(String xpath, String text) {
+        String ele=getDriver().findElement(By.xpath(xpath)).getText();
+       assertTrue("The string contains value", ele.contains(text));
+       assertTrue("skks",ele.endsWith("t"));
+
     }
 }
--- src/test/resources/features/predifined.feature
+++ src/test/resources/features/predifined.feature
@@ -2,4 +2,10 @@
 Feature: Google Search
 
   Scenario: search functionality test
-    Given user navigates to "https://www.google.com"
+    Given I navigate to "https://www.google.com"
+    Then title should be "Google"
+    And element with xpath "//*[@name='q']" should be present
+    When I type "Behavior Driven Development" into element with xpath "//*[@name='q']"
+    And I click on element with xpath "//div[@class='lJ9FBc']//input[@name='btnK']"
+    And I wait for element with xpath "//h3[normalize-space()='Behavior-driven development']" to be present
+    Then element with xpath "//h3[normalize-space()='Behavior-driven development']" should contain text "development"
